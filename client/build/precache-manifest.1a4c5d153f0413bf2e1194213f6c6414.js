dc7b512353e1ec9c663657b7b955ce924c3374fd
Nyanye htun <nyanyehtun@my.jcu.edu.au>
2020-04-17 21:05:03 +0800

    Update client/.gitingore

---
+++ client/build/precache-manifest.1a4c5d153f0413bf2e1194213f6c6414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73a49816547504162e11f4622df3f3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6f001ac9e774b266fe7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6a65d2b41fd11f1b336b",
+    "url": "/static/js/2.189cac9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.189cac9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6f001ac9e774b266fe7",
+    "url": "/static/js/main.f09ae741.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
